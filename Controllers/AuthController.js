0b079928f189c3d43d25cbcb477a8c7742a23797
Manish Kumar <manishgupta3859@gmail.com>
2025-11-13 10:54:07 +0530

    authentication changed

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -78,19 +78,19 @@ module.exports.login = async (req, res, next) => {
 };
 
 // //  USER VERIFICATION
-// module.exports.userVerification = (req, res) => {
-//   const token = req.cookies.token;
-//   if (!token) {
-//     return res.json({ status: false });
-//   }
+module.exports.userVerification = (req, res) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.json({ status: false });
+  }
 
-//   jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
-//     if (err) {
-//       return res.json({ status: false });
-//     } else {
-//       const user = await User.findById(data.id);
-//       if (user) return res.json({ status: true, user: user.username });
-//       else return res.json({ status: false });
-//     }
-//   });
-// };
+  jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
+    if (err) {
+      return res.json({ status: false });
+    } else {
+      const user = await User.findById(data.id);
+      if (user) return res.json({ status: true, user: user.username });
+      else return res.json({ status: false });
+    }
+  });
+};
